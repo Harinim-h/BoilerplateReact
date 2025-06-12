ce7b0e716743b050dc767342d8bf4e60a44e8521
Harinim-h <harinim378@gmail.com>
2025-06-12 15:35:18 +0530

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'light-blue': "url('https://wallpapers.com/images/hd/plain-light-blue-background-2000-x-1997-7zhb45hop2v9m4by.jpg')",
+      },
+    },
   },
   plugins: [],
 };
